617ee7abbdb9baaee4ea474d8791f9383aa6cf9b
magugolenc <145765394+magugolenc@users.noreply.github.com>
2023-11-16 16:50:22 +0100

    Add files via upload

---
+++ Actividad_1.java
@@ -0,0 +1,17 @@
+/*Ejercicio 1*/
+
+import java.util.Scanner;
+
+public class Actividad_1 {
+public static void eco(int n) {
+	for (int i = 0;i<n;i++) {
+		System.out.println("Eco...");
+	}
+}
+public static void main (String []args) {
+	Scanner tc = new Scanner (System.in);
+	System.out.println("¿Cuanto eco hay en tu cueva?");
+	int numEco=tc.nextInt();
+	eco(numEco);
+}
+}
